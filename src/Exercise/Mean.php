19c42bc026e50dd6a31f57a7d3aed076a8f84c7e
Aydin Hassan <aydin@hotmail.co.uk>
2016-05-03 23:23:43 +0200

    PSR2 Self Check exercise

--- src/Exercise/Mean.php
+++ src/Exercise/Mean.php
@@ -6,8 +6,12 @@ use PhpSchool\PhpWorkshop\Exercise\AbstractExercise;
 use PhpSchool\PhpWorkshop\Exercise\CliExercise;
 use PhpSchool\PhpWorkshop\Exercise\ExerciseInterface;
 use PhpSchool\PhpWorkshop\Exercise\ExerciseType;
+use PhpSchool\PhpWorkshop\ExerciseCheck\SelfCheck;
+use PhpSchool\PhpWorkshop\Result\Failure;
+use PhpSchool\PhpWorkshop\Result\ResultInterface;
+use PhpSchool\PhpWorkshop\Result\Success;
 
-class Mean extends AbstractExercise implements ExerciseInterface, CliExercise
+class Mean extends AbstractExercise implements ExerciseInterface, CliExercise, SelfCheck
 {
 
     /**
@@ -47,5 +51,22 @@ class Mean extends AbstractExercise implements ExerciseInterface, CliExercise
     public function getType()
     {
         return ExerciseType::CLI();
+    }
+
+    /**
+     * @param string $fileName
+     * @return ResultInterface
+     */
+    public function check($fileName)
+    {
+        $phpCsBinary = __DIR__ . '/../../vendor/bin/phpcs';
+        $cmd = sprintf('%s %s --standard=PSR2', $phpCsBinary, $fileName);
+        exec($cmd, $output, $exitCode);
+
+        if ($exitCode === 0) {
+            return new Success('PSR2 Code Check');
+        }
+
+        return new Failure('PSR2 Code Check', 'Coding style did not conform to PSR2!');
     }
 }
