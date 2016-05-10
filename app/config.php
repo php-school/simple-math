e4031d5b730ee42d9616681c721ad4120fef2e56
Aydin Hassan <aydin@hotmail.co.uk>
2016-05-10 22:48:46 +0200

    Custom PSR2 Simple Check

--- app/config.php
+++ app/config.php
@@ -3,14 +3,15 @@
 use function DI\factory;
 use function DI\object;
 use Interop\Container\ContainerInterface;
-use PhpSchool\SimpleMath\Exercise\GetExercise;
+use PhpSchool\SimpleMath\Check\Psr2Check;
 use PhpSchool\SimpleMath\Exercise\Mean;
-use PhpSchool\SimpleMath\Exercise\PostExercise;
-use PhpSchool\SimpleMath\MyFileSystem;
+use Symfony\Component\Filesystem\Filesystem;
 
 return [
     //Define your exercise factories here
     Mean::class => factory(function (ContainerInterface $c) {
-        return new Mean($c->get(\Symfony\Component\Filesystem\Filesystem::class));
+        return new Mean($c->get(Filesystem::class));
     }),
+
+    Psr2Check::class => object(),
 ];
