ea20f528042e7a191ded295c3ad1eefe070e092b
Aydin Hassan <aydin@hotmail.co.uk>
2016-12-09 23:42:13 +0100

    Update to php-school/php-workshop v2

--- src/Exercise/Mean.php
+++ src/Exercise/Mean.php
@@ -31,14 +31,14 @@ class Mean extends AbstractExercise implements ExerciseInterface, CliExercise
      */
     public function getArgs()
     {
-        $numArgs = rand(0, 10);
+        $numArgs = rand(1, 10);
 
         $args = [];
         for ($i = 0; $i < $numArgs; $i ++) {
             $args[] = rand(0, 100);
         }
 
-        return $args;
+        return [$args];
     }
 
     /**
